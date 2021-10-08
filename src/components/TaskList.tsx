135ca9960e927f06f47a671583a093b967282075
pedro-drosa <pedro_drosa@hotmail.com>
2021-10-08 07:30:23 -0300

    feature: handleCreateNewTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,14 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim() !== '') {
+      let currentTask = {
+        id: Date.now(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks, currentTask])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
